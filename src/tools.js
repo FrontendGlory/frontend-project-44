cdca0de26d5430ccd4deb643e822b9c861955e74
Vyacheslav Erey <slavaerey@gmail.com>
2023-10-03 13:21:37 +0300

    fix

--- src/tools.js
+++ src/tools.js
@@ -1,6 +1,6 @@
 const getRandomNumber = (min = 1, max = 100) => {
-    const result = Math.floor(Math.random() * (max - min) + min);
-    return result;
+  const result = Math.floor(Math.random() * (max - min) + min);
+  return result;
 };
 
 export default getRandomNumber;
